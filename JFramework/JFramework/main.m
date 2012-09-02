cfc99868e7ecd8b8028c6a715dcdbe30e7933540
Jimmy Spencer <jimmyspencer@Jimmys-MacBook-Pro.local>
2012-09-01 22:27:58 -0700

    Starting out

---
+++ JFramework/JFramework/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JFramework
+//
+//  Created by Jimmy Spencer on 9/1/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
