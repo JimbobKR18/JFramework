b488f49f20695be0055d6e0e39903a007dc17ba2
Jimmy Spencer <jimbob@dawghouse.jimmy>
2013-03-04 20:46:22 -0800

    Files added yay.

---
+++ Source/Managers/DebugManager.h
@@ -0,0 +1,46 @@
+/*
+ * DebugManager.h
+ *
+ *  Created on: Feb 16, 2013
+ *      Author: jimbob
+ */
+
+#ifndef DEBUGMANAGER_H_
+#define DEBUGMANAGER_H_
+
+#include "Manager.h"
+#include "AntTweakBar.h"
+#include "Common.h"
+
+#if defined(_WIN32) || defined(__APPLE__)
+  #include "SDL.h"
+#else
+  #include <SDL/SDL.h>
+#endif
+
+class DebugManager : public Manager
+{
+private:
+  std::map<std::string, TwBar*>   mTweakBars;
+public:
+  DebugManager(GameApp *aApp);
+  ~DebugManager();
+
+  void                AddTweakBar(std::string const &aName);
+  void                AddInt(std::string const &aTweakBarName,
+                             std::string const &aVarName,
+                             int *aVariable);
+  void                AddBool(std::string const &aTweakBarName,
+                               std::string const &aVarName,
+                               bool *aVariable);
+  void                AddFloat(std::string const &aTweakBarName,
+                               std::string const &aVarName,
+                               float *aVariable);
+  int                 HandleEvent(SDL_Event &event);
+
+  void                Update();
+  void                SendMessage(Message const &aMessage);
+  static std::string  GetName() {return "DebugManager";}
+};
+
+#endif /* DEBUGMANAGER_H_ */
