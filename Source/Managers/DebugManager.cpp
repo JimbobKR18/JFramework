1a4c5e4454aff4adf32eca2b4eb45d1c0e30cb93
Jimmy Spencer <jimmyspencer@Jimmys-MacBook-Pro.local>
2013-03-17 18:03:30 -0700

    XCode update

--- Source/Managers/DebugManager.cpp
+++ Source/Managers/DebugManager.cpp
@@ -10,48 +10,49 @@
 
 DebugManager::DebugManager(GameApp *aApp) : Manager(aApp, "DebugManager")
 {
-  TwInit(TW_OPENGL, NULL);
-  Screen *screen = GetOwningApp()->GET<GraphicsManager>()->GetScreen();
-  TwWindowSize(screen->GetWidth(),screen->GetHeight());
+  //TwInit(TW_OPENGL, NULL);
+  //Screen *screen = GetOwningApp()->GET<GraphicsManager>()->GetScreen();
+  //TwWindowSize(screen->GetWidth(),screen->GetHeight());
 }
 
 DebugManager::~DebugManager()
 {
-  TwTerminate();
+  //TwTerminate();
   mTweakBars.clear();
 }
 
 void DebugManager::AddTweakBar(std::string const &aName)
 {
-  TwBar *bar = TwNewBar(aName.c_str());
-  mTweakBars.insert(std::pair<std::string, TwBar*>(aName, bar));
+  //TwBar *bar = TwNewBar(aName.c_str());
+  //mTweakBars.insert(std::pair<std::string, TwBar*>(aName, bar));
 }
 void DebugManager::AddInt(std::string const &aTweakBarName,
                           std::string const &aVarName,
                           int *aVariable)
 {
-  TwAddVarRW(mTweakBars[aTweakBarName], aVarName.c_str(), TW_TYPE_INT32, aVariable, "");
+  //TwAddVarRW(mTweakBars[aTweakBarName], aVarName.c_str(), TW_TYPE_INT32, aVariable, "");
 }
 void DebugManager::AddBool(std::string const &aTweakBarName,
                            std::string const &aVarName,
                            bool *aVariable)
 {
-  TwAddVarRW(mTweakBars[aTweakBarName], aVarName.c_str(), TW_TYPE_BOOLCPP, aVariable, "");
+  //TwAddVarRW(mTweakBars[aTweakBarName], aVarName.c_str(), TW_TYPE_BOOLCPP, aVariable, "");
 }
 void DebugManager::AddFloat(std::string const &aTweakBarName,
                             std::string const &aVarName,
                             float *aVariable)
 {
-  TwAddVarRW(mTweakBars[aTweakBarName], aVarName.c_str(), TW_TYPE_FLOAT, aVariable, "");
+  //TwAddVarRW(mTweakBars[aTweakBarName], aVarName.c_str(), TW_TYPE_FLOAT, aVariable, "");
 }
 int DebugManager::HandleEvent(SDL_Event &event)
 {
-  return TwEventSDL(&event, SDL_MAJOR_VERSION, SDL_MINOR_VERSION);
+  return 0;
+  //return TwEventSDL(&event, SDL_MAJOR_VERSION, SDL_MINOR_VERSION);
 }
 
 void DebugManager::Update()
 {
-  TwDraw();
+  //TwDraw();
 }
 void DebugManager::SendMessage(Message const &aMessage)
 {
